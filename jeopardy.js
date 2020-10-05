296ace1e48f7ef8dafbb0b53438cb3702f351b3d
Chris <chris.risser@yahoo.com>
2020-10-05 13:21:25 -0700

    Need to implement dynamic addition of columns

--- jeopardy.js
+++ jeopardy.js
@@ -2,8 +2,11 @@ $(document).ready(()=>{
   $('#category').click(()=>{
     getCategory();
   });
+  $('#col-options').change(()=>{
+  let a = $('#col-options').val();
+  alert(a);
 });
-
+});
 async function fetchCategoryJSON() {
   const response = await fetch('http:jservice.io/api/categories?count=100');
   const answers = await response.json();
@@ -12,11 +15,10 @@ async function fetchCategoryJSON() {
 function getCategory(){
   let initialvalue = 200;
   for(let i = 0;i<5; i++){
-
        console.log(`#${i}`);
         $(`#${i}`).html(`$${initialvalue}`);
         initialvalue+=200;
-  
+
   }
 fetchCategoryJSON().then(answers => {
 createTitle(answers);
@@ -26,7 +28,6 @@ createTitle(answers);
 function createTitle(ans){
   //create randomnumber between 1-100 to get random category from api
   const randomnumber = Math.floor(Math.random()*(100-1)+1);
-  //console.log(ans[randomnumber].id);
   document.getElementById('category').innerHTML = ans[randomnumber].title;
   fetchQandAs(ans[randomnumber].id);
 }
@@ -37,12 +38,15 @@ async function fetchQandAs(id){
   const answers = await response.json();
   loadQuestions(answers);
 }
+/*Function that load the questions and answers into the Jeopardy
+buttons*/
 function loadQuestions(answers){
   for(let i = 0;i<answers.length; i++){
-
      $(`#${i}`).click(()=>{
-       console.log(`#${i}`);
         $(`#${i}`).html(answers[i].question);
+        setTimeout(()=>{
+          $(`#${i}`).html(answers[i].answer);
+        },7000)
      });
   }
 }
