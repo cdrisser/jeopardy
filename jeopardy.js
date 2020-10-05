617e71b3641c4b2c5fcbcdf06360f7a8cf310b85
Chris <chris.risser@yahoo.com>
2020-10-05 07:54:38 -0700

    skeleton files for HTML, CSS, JS created

--- jeopardy.js
+++ jeopardy.js
@@ -1,15 +1,49 @@
+$(document).ready(()=>{
+  $('#category').click(()=>{
+    getCategory();
+  });
+});
 
 async function fetchCategoryJSON() {
-  const response = await fetch('http://jservice.io/api/categories?count=1');
+  const response = await fetch('http:jservice.io/api/categories?count=100');
   const answers = await response.json();
   return answers;
 }
+function getCategory(){
+  let initialvalue = 200;
+  for(let i = 0;i<5; i++){
 
+       console.log(`#${i}`);
+        $(`#${i}`).html(`$${initialvalue}`);
+        initialvalue+=200;
+  
+  }
 fetchCategoryJSON().then(answers => {
-  console.log(answers);
-createBoard(answers);
+createTitle(answers);
 
 });
-function createBoard(ans){
-  document.getElementById('category').innerHTML = ans.title;
 }
+function createTitle(ans){
+  //create randomnumber between 1-100 to get random category from api
+  const randomnumber = Math.floor(Math.random()*(100-1)+1);
+  //console.log(ans[randomnumber].id);
+  document.getElementById('category').innerHTML = ans[randomnumber].title;
+  fetchQandAs(ans[randomnumber].id);
+}
+async function fetchQandAs(id){
+  const fetchstr = `http://jservice.io/api/clues?category=${id}`
+  console.log(fetchstr);
+  const response = await fetch(fetchstr);
+  const answers = await response.json();
+  loadQuestions(answers);
+}
+function loadQuestions(answers){
+  for(let i = 0;i<answers.length; i++){
+
+     $(`#${i}`).click(()=>{
+       console.log(`#${i}`);
+        $(`#${i}`).html(answers[i].question);
+     });
+  }
+}
+getCategory();
