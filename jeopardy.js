a3e0221301d879a5f00c36e6dd3b29c7da9a445e
Chris <chris.risser@yahoo.com>
2020-11-10 21:07:35 -0700

    updated all 3 files

--- jeopardy.js
+++ jeopardy.js
@@ -1,53 +1,66 @@
+
 $(document).ready(()=>{
-  $('#category').click(()=>{
-    getCategory();
-  });
   $('#col-options').change(()=>{
-  let a = $('#col-options').val();
-  alert(a);
+//remove column in case it middle of Game
+$('.col').remove();
+//send number of column requested to function to dynamicaly load
+
+  addCols(Number($('#col-options').val()));
+  });
 });
-});
+
+async function fetchQandAs(id,i){
+  const fetchstr = `http://jservice.io/api/clues?category=${id}`
+  console.log(fetchstr);
+  const response = await fetch(fetchstr);
+  const answers = await response.json();
+  loadQuestions(answers,i);
+}
+/*Function that load the questions and answers into the Jeopardy
+buttons*/
+function loadQuestions(answers,colnum){
+  for(let i = 0;i<answers.length; i++){
+     $(`#category${colnum}-${i}`).click(()=>{
+        $(`#category${colnum}-${i}`).html(answers[i].question);
+        setTimeout(()=>{
+          $(`#category${colnum}-${i}`).html(answers[i].answer);
+        },7000)
+     });
+  }
+}
+function addCols(numCols){
+  console.log(numCols);
+  let intamt = 200;
+  for(let i=0;i<numCols;i++){
+    let newdiv = $(`<div class=col></div>`);
+    let butcat = $(`<button id=category${i}></button>`);
+    newdiv.append(butcat);
+    for(let j=0;j<5;j++){
+      let but = $(`<button id=category${i}-${j}>$${intamt}</button>`);
+      newdiv.append(but);
+      intamt+=200;
+    }
+    intamt = 200;
+    $("#each-col").append(newdiv);
+  }
+  fetchCategoryJSON().then(answers => {
+  createTitle(answers, numCols);
+
+  });
+}
 async function fetchCategoryJSON() {
   const response = await fetch('http:jservice.io/api/categories?count=100');
   const answers = await response.json();
   return answers;
 }
-function getCategory(){
-  let initialvalue = 200;
-  for(let i = 0;i<5; i++){
-       console.log(`#${i}`);
-        $(`#${i}`).html(`$${initialvalue}`);
-        initialvalue+=200;
 
-  }
-fetchCategoryJSON().then(answers => {
-createTitle(answers);
-
-});
-}
-function createTitle(ans){
+function createTitle(ans, numCols){
+  console.log(ans)
   //create randomnumber between 1-100 to get random category from api
+  for(let i =0; i<numCols; i++){
+    console.log(numCols);
   const randomnumber = Math.floor(Math.random()*(100-1)+1);
-  document.getElementById('category').innerHTML = ans[randomnumber].title;
-  fetchQandAs(ans[randomnumber].id);
+  document.getElementById(`category${i}`).innerHTML = ans[randomnumber].title;
+  fetchQandAs(ans[randomnumber].id, i );
 }
-async function fetchQandAs(id){
-  const fetchstr = `http://jservice.io/api/clues?category=${id}`
-  console.log(fetchstr);
-  const response = await fetch(fetchstr);
-  const answers = await response.json();
-  loadQuestions(answers);
 }
-/*Function that load the questions and answers into the Jeopardy
-buttons*/
-function loadQuestions(answers){
-  for(let i = 0;i<answers.length; i++){
-     $(`#${i}`).click(()=>{
-        $(`#${i}`).html(answers[i].question);
-        setTimeout(()=>{
-          $(`#${i}`).html(answers[i].answer);
-        },7000)
-     });
-  }
-}
-getCategory();
